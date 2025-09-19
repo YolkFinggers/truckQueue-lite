2a30b2277e324766dd4db87de6774bc261b64f54
YolkFinggers <134028687+YolkFinggers@users.noreply.github.com>
2025-09-19 13:48:41 +0800

    Electron version
    - No new updates in UI/UX (same as lite)
    - No any other browser required instead
    - Only Windows & Linux supported

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
+const os = require('os');
 
 let assignWin;
 let displayWin;
@@ -11,10 +12,16 @@ function createWindows() {
 
   // Assign window (fullscreen)
   assignWin = new BrowserWindow({
-    simpleFullscreen: false,
+    fullscreen: false,
+    show: false,
     webPreferences: { contextIsolation: true },
   });
   assignWin.loadFile(path.join(__dirname, 'src', 'index.html'));
+  
+  assignWin.once('ready-to-show', () => {
+  assignWin.maximize(); // maximize window
+  assignWin.show();     // then show it
+});
 
   // Display window (kiosk on second monitor)
   displayWin = new BrowserWindow({
@@ -28,7 +35,22 @@ function createWindows() {
   displayWin.loadFile(path.join(__dirname, 'src', 'display.html'));
 }
 
-app.whenReady().then(createWindows);
+function enableAutoStart() {
+  const platforms = ['win32', 'darwin', 'linux'];
+  if (platforms.includes(process.platform)) {
+    app.setLoginItemSettings({
+      openAtLogin: true,
+      path: process.execPath, // current Electron executable
+    });
+  }
+}
+
+app.whenReady().then(() => {
+  enableAutoStart();
+  createWindows();
+});
+
+app.setPath('userData', path.join(os.homedir(), 'TruckQueueAppData'));
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
